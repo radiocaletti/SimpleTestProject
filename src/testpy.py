5d501a4433defc8f4e318a125452beafb024ac23
tcaletti <t@mac>
2023-05-22 14:02:41 +0200

    Hashing.

--- src/testpy.py
+++ src/testpy.py
@@ -1,1 +1,12 @@
 print("Hello, this is Python.")
+
+import hashlib
+
+algos = hashlib.algorithms_available
+for algo in algos:
+    print(algo)
+
+sha256hash = hashlib.sha256()
+sha256hash.update(b"hello")
+#print(sha256hash.digest())
+print(sha256hash.hexdigest())
